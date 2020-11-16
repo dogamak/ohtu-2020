5f44af33f32203a7d5e9ce688a8029c869c21416
Mitja Karhusaari <mitja@karhusaari.me>
2020-11-16 22:45:22 +0200

    Implement tests for registration

--- week3/WebLogin/src/test/java/ohtu/Stepdefs.java
+++ week3/WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -22,6 +22,13 @@ public class Stepdefs {
         WebElement element = driver.findElement(By.linkText("login"));       
         element.click();   
     }    
+
+    @Given("command new user is selected")
+    public void commandNewUserIsSelected() {
+        driver.get(baseUrl);
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+    }
     
     @When("correct username {string} and password {string} are given")
     public void correctUsernameAndPasswordAreGiven(String username, String password) {
@@ -44,9 +51,39 @@ public class Stepdefs {
         pageHasContent("Give your credentials to login");
     }    
 
-    @Then("nonexistent username {string} and password {string} are given")
+    @When("nonexistent username {string} and password {string} are given")
     public void nonexistentUsernameAndPasswordAreGiven(String username, String password) {
       logInWith(username, password);
+    }
+
+    @When("a valid username {string} and valid password {string} and matching password confirmation are entered")
+    public void anValidUsernameAndValidPasswordAndMatchingPasswordConfirmationAreEntered(String username, String password) {
+      registerWith(username, password, true);
+    }
+
+    @When("an invalid username {string} and valid password {string} and matching password confirmation are entered")
+    public void anInvalidUsernameAndValidPasswordAndMatchingPasswordConfirmationAreEntered(String username, String password) {
+      registerWith(username, password, true);
+    }
+
+    @When("a valid username {string} and invalid password {string} and matching password confirmation are entered")
+    public void aValidUsernameAndInvalidPasswordAndMatchingPasswordConfirmationAreEntered(String username, String password) {
+      registerWith(username, password, true);
+    }
+
+    @When("a valid username {string} and valid password {string} and non-matching password confirmation are entered")
+    public void aValidUsernameAndValidPasswordAndNonMatchingPasswordConfirmationAreEntered(String username, String password) {
+      registerWith(username, password, false);
+    }
+
+    @Then("user is not created and error {string} is reported")
+    public void userIsNotCreatedAndErrorIsReported(String error) {
+      pageHasContent(error);
+    }
+
+    @Then("a new user is created")
+    public void aNewUserIsCreated() {
+      pageHasContent("Welcome to Ohtu Application!");
     }
     
     @After
@@ -69,4 +106,25 @@ public class Stepdefs {
         element = driver.findElement(By.name("login"));
         element.submit();  
     } 
+
+    private void registerWith(
+        String username,
+        String password,
+        boolean passwordConfirmationMatches
+    ) {
+        assertTrue(driver.getPageSource().contains("Create username and give password"));
+        WebElement element = driver.findElement(By.name("username"));
+        element.sendKeys(username);
+        element = driver.findElement(By.name("password"));
+        element.sendKeys(password);
+
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys(password);
+
+        if (!passwordConfirmationMatches)
+          element.sendKeys("garbage");
+
+        element = driver.findElement(By.name("signup"));
+        element.submit();  
+    }
 }
--- week3/WebLogin/src/test/resources/ohtu/registration.feature
+++ week3/WebLogin/src/test/resources/ohtu/registration.feature
@@ -2,7 +2,7 @@ Feature: A new user account can be created if a proper unused username and passw
 
     Scenario: creation is successful with valid username and password
         Given command new user is selected
-        When  a valid username "liisa" and password "salainen1" and matching password confirmation are entered
+        When  a valid username "liisa" and valid password "salainen1" and matching password confirmation are entered
         Then  a new user is created
 
     Scenario: creation fails with too short username and valid password
